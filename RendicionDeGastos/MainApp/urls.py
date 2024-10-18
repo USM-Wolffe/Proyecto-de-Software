05ede23574920ba41dceb12dfc12beb5c07c5703
USM-Wolffe <117790628+USM-Wolffe@users.noreply.github.com>
2024-10-18 07:14:01 +0000

    Vistas contador, trabajador,jefe,index, rendicion e ingresar realizadas

--- RendicionDeGastos/MainApp/urls.py
+++ RendicionDeGastos/MainApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, trabajador, contador
+from .views import index, trabajador, contador, jefe, rendicion,ingresar
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('trabajador/', trabajador, name='trabajador'),
-    path('contador/', contador, name='contador')
+    path('', index, name='index'),  # Ruta para la página de inicio
+    path('trabajador/', trabajador, name='trabajador'),  # Ruta para la vista del trabajador
+    path('contador/', contador, name='contador'),  # Ruta para la vista del contador
+    path('jefe/', jefe, name='jefe'),  # Ruta para la vista del jefe
+    path('rendicion/', rendicion, name='rendicion'),  
+    path('ingresar/', ingresar, name='ingresar'),  
 ]
--- RendicionDeGastos/MainApp/views.py
+++ RendicionDeGastos/MainApp/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# Vista para la página de inicio
 def index(request):
     return render(request, 'MainApp/index.html')
 
+# Vista para el trabajador
 def trabajador(request):
-    return render(request, "MainApp/trabajador.html")
+    return render(request, 'MainApp/trabajador.html')
 
+# Vista para el contador
 def contador(request):
-    return render(request, "MainApp/contador.html")
+    return render(request, 'MainApp/contador.html')
+
+# Vista para el jefe de área
+def jefe(request):
+    return render(request, 'MainApp/jefe.html')
+
+# Vista para la rendicion
+def rendicion(request):
+    return render(request, 'MainApp/rendicion.html')
+
+# Vista para la rendicion
+def ingresar(request):
+    return render(request, 'MainApp/ingresar.html')
